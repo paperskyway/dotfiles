963fef641d077fb8f61f48bc6cb50eb1e848e497
Andy Tung <andyytung@gmail.com>
2016-07-06 15:58:11 -0700

    zsh

--- syncdotfiles.sh
+++ syncdotfiles.sh
@@ -2,5 +2,6 @@
 
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.inputrc ~/.inputrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/sublimepreferences/User/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
