fdd26d857ad86c63cd956fb2738500d37d7c6bd2
Andy Tung <andyytung@gmail.com>
2016-07-18 12:21:12 -0700

    sync dot file

--- syncdotfiles.sh
+++ syncdotfiles.sh
@@ -6,3 +6,4 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.atom/ ~/.atom
 ln -s ~/dotfiles/sublimepreferences/User/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+git clone https://github.com/robbyrussell/oh-my-zsh.git
