69840c28a2f47757cc6a4c4378c2207b493e331e
Andy Tung <andyytung@gmail.com>
2016-09-08 13:11:11 -0700

    change syncdotfiles to move files instead of delete

--- syncdotfiles.sh
+++ syncdotfiles.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+mv ~/.gitconfig ~/.gitconfig-old
+mv ~/.inputrc ~/.inputrc-old
+mv ~/.zshrc ~/.zshrc-old
+mv ~/.bash_profile ~/.bash_profile-old
+mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User-old 
 
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.inputrc ~/.inputrc
