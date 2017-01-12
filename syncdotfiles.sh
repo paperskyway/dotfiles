00698f0d30ea92e95b81e528eb502865a088cc8f
Andy Tung <andyytung@gmail.com>
2017-01-11 22:37:34 -0800

    added vimrc

--- syncdotfiles.sh
+++ syncdotfiles.sh
@@ -4,6 +4,7 @@ mv ~/.inputrc ~/.inputrc-old
 mv ~/.zshrc ~/.zshrc-old
 mv ~/.bash_profile ~/.bash_profile-old
 mv ~/.hyper.js ~/.hyper.js-old
+mv ~/vimrc ~/vimrc-old
 mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User-old 
 
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
@@ -11,6 +12,7 @@ ln -s ~/dotfiles/.inputrc ~/.inputrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.hyper.js ~/.hyper.js
+ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.hyper_plugins/ ~/.hyper_plugins/
 ln -s ~/dotfiles/sublimepreferences/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 git clone https://github.com/robbyrussell/oh-my-zsh.git
