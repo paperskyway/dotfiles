dcd6ba360f7a753cb4f31a2380ad0c499128bdc8
Andy Tung <andyytung@gmail.com>
2017-04-17 21:57:25 -0700

    removed dot from name

--- syncdotfiles.sh
+++ syncdotfiles.sh
@@ -5,13 +5,14 @@ rm ~/.zshrc
 rm ~/.bash_profile
 rm ~/.hyper.js
 rm ~/.vimrc
+rm ~/.config/nvim/init.vim
 
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.inputrc ~/.inputrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.hyper.js ~/.hyper.js
-ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.hyper_plugins/ ~/.hyper_plugins/
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/inputrc ~/.inputrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/hyper.js ~/.hyper.js
+ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/hyper_plugins/ ~/.hyper_plugins/
 git clone https://github.com/robbyrussell/oh-my-zsh.git
