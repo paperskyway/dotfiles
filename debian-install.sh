c7159d248783f3f518deb3669d7c3c0244f8393b
Andy Tung <andyytung@gmail.com>
2017-06-09 00:52:49 +0000

    updated script

--- debian-install.sh
+++ debian-install.sh
@@ -4,14 +4,10 @@ curl -fLo ~/.vim/colors/molokai.vim --create-dirs https://raw.githubusercontent.
 rm ~/.gitconfig
 rm ~/.inputrc
 rm ~/.zshrc
-rm ~/.bashrc
 rm ~/.vimrc
-mkdir -p ~/.config/nvim
-rm ~/.config/nvim/init.vim
 
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/inputrc ~/.inputrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 git clone https://github.com/robbyrussell/oh-my-zsh.git
