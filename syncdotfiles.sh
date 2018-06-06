9b7650bacbc9f306ab8fee1d6279d210bbaf5ef5
Andy Tung <andytung@uber.com>
2018-06-06 12:09:15 -0700

    removed nvim

--- syncdotfiles.sh
+++ syncdotfiles.sh
@@ -2,13 +2,12 @@
 rm ~/.gitconfig
 rm ~/.inputrc
 rm ~/.zshrc
+rm ~/.vimrc
 rm ~/.bash_profile
-rm ~/.config/nvim/init.vim
 
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/inputrc ~/.inputrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
-mkdir -p ~/.config/nvim/
-ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
 git clone https://github.com/robbyrussell/oh-my-zsh.git
